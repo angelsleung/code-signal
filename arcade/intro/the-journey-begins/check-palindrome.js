4694d77827e8169986286a79f82d1790fb9839bc
Angel Leung <angelhleung@gmail.com>
2021-05-28 23:28:08 -0700

    Completed checkPalindrome

---
+++ arcade/intro/the-journey-begins/check-palindrome.js
@@ -0,0 +1,8 @@
+function checkPalindrome(inputString) {
+  for (let i = 0; i < inputString.length / 2; i++) {
+    returninputString[i] !== inputString[inputString.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
